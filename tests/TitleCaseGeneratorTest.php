91f91742f963b161f7e62479d903e60d048fc980
Ben Casalino & Sarah McEwing <student@epicodus.com>
2015-08-10 10:36:12 -0700

    both tests pass

--- tests/TitleCaseGeneratorTest.php
+++ tests/TitleCaseGeneratorTest.php
@@ -20,6 +20,19 @@
             $this->assertEquals("Beowulf", $result);
         }
 
+        function test_makeTitleCase_multipleWords()
+        {
+
+            // Arrange
+            $test_TitleCaseGenerator = new TitleCaseGenerator;
+            $input = "the little mermaid";
+
+            // Act
+            $result = $test_TitleCaseGenerator->makeTitleCase($input);
+
+            // Assert
+            $this->assertEquals("The Little Mermaid", $result);
+        }
     }
 
 ?>
